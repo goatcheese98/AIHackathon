d65157f4185b8b479a30e37b4b0be5e2fc3beea8
Rohan Jasani <jasani.rohan@gmail.com>
2025-11-28 23:16:52 -0800

    Fix routing with HashRouter and add GitHub Actions deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { Layout } from './components/Layout';
 import { Home } from './pages/Home';
@@ -11,7 +11,7 @@ import { Settings } from './pages/Settings';
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -22,7 +22,7 @@ function App() {
             <Route path="/settings" element={<Settings />} />
           </Routes>
         </Layout>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 }
